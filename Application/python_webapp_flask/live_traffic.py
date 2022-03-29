3e908b7eb2aee53cf700b65144acbf2d1c151262
robert0628 <robertcarnesdev@gmail.com>
2022-03-29 07:59:05 -0400

    Cleaned up live_traffic.py and app_rationalization.py

--- Application/python_webapp_flask/live_traffic.py
+++ Application/python_webapp_flask/live_traffic.py
@@ -4,7 +4,6 @@ import datetime
 from colour import Color
 from netaddr import IPNetwork
 from .RCAA_functions import generate_token
-from requests.auth import HTTPBasicAuth
 
 networks_url = 'https://71.25.48.227/api/fmc_config/v1/domain/{}/object/networks?limit=1000&expanded=true'
 
@@ -21,14 +20,9 @@ def get_networks():
 
     return networks_response
 
-def auto_graph_generate_nodes(nodes, json_file):
-    incoming_data = {}
-    outgoing_data = {}
-    incoming_connections = {}
-    outgoing_connections = {}
-    networks = {}
-
+def get_subnets():
     networks_response = get_networks()
+    networks = {}
 
     for item in networks_response['items']:
         addresses = []
@@ -44,6 +38,10 @@ def auto_graph_generate_nodes(nodes, json_file):
             addresses.append(str(elem))
         
         networks[item['value']] = addresses
+    return networks
+    
+def auto_graph_generate_nodes(nodes, json_file):
+    incoming_data = outgoing_data = incoming_connections = outgoing_connections = {}
 
     for item in json_file['items']:
         if item['destinationIp'] not in incoming_data.keys():
@@ -59,24 +57,24 @@ def auto_graph_generate_nodes(nodes, json_file):
         else:
             outgoing_data[item['sourceIp']] += item['bytesSent']
             outgoing_connections[item['sourceIp']] += 1
+            
+    return convert_nodes(nodes=nodes, items=json_file['items'], incoming_data=incoming_data, incoming_connections=incoming_connections, outgoing_data=outgoing_data, outgoing_connections=outgoing_connections)
 
+def convert_nodes(nodes, items, incoming_data, incoming_connections, outgoing_data, outgoing_connections):
+    networks = get_subnets()
     for node in nodes:
-        del node['connections']
-        del node['incoming']
-        del node['outgoing']
-        del node['svg']
+        keys = ['connections', 'incoming', 'outgoing', 'svg']
+        for key in keys:
+            del node[key]
 
         node['status'] = "online"
         node['ip'] = node['id']
         node['id'] = 'HostUUID'
         #we can't get hostUUID as of now
 
-        #initialization
-        node['nodeName'] = ''
-        protocol = ''
-        sourcePort = ''
-        node['macAddress'] = ''
-        for item in json_file['items']:
+        node['nodeName'] = protocol = sourcePort = node['macAddress'] = ''
+        
+        for item in items:
             if item['sourceIp'] == node['ip']:
                 node['macAddress'] = item['srcMAC']
                 protocol = item['protocol']
@@ -86,13 +84,7 @@ def auto_graph_generate_nodes(nodes, json_file):
             if item['destinationIp'] == node['ip']:
                 node['macAddress'] = item['dstMAC']
 
-        addresses = []
-
-        network = IPNetwork('/'.join([node['ip'], '24']))
-        #TODO:find proper mask
-        generator = network.iter_hosts()
-        for elem in generator:
-            addresses.append(str(elem))
+        addresses = get_ips(node['ip'])
 
         lst_ip = addresses[0].split('.') #TODO
         lst_ip[-1] = '0'
@@ -125,7 +117,21 @@ def auto_graph_generate_nodes(nodes, json_file):
         except:
             sourcePort = ''
 
-        node['port'] = {
+        node = set_device_data(node, protocol, sourcePort)
+
+    return nodes
+
+def get_ips(ip):
+    addresses = []
+    network = IPNetwork('/'.join([ip, '24']))
+    #TODO:find proper mask
+    generator = network.iter_hosts()
+    for elem in generator:
+        addresses.append(str(elem))
+    return addresses
+
+def set_device_data(node, protocol, sourcePort):
+    node['port'] = {
             "id": "protocoPortObjUUID",
             "name": "protocolport_obj",
             "type": "ProtocolPortObject",
@@ -133,161 +139,29 @@ def auto_graph_generate_nodes(nodes, json_file):
             "port": sourcePort
           }
 
-        if (node['l_outgoingConn'] == 0 and node['l_incomingConn'] > 0 and node['l_outgoingData'] == 0):
-            objectType = 'Server'
-        else:
-            objectType = 'Client'
+    if (node['l_outgoingConn'] == 0 and node['l_incomingConn'] > 0 and node['l_outgoingData'] == 0):
+        objectType = 'Server'
+    else:
+        objectType = 'Client'
 
-        node['objectType'] = objectType #TODO
-        node['os'] = ''
-        node['domain'] = ''
-        node['deviceDetails'] = {
-          "cpuType": "unknown",
-          "cpuCores": "unknown",
-          "memoryInMB": "unknown",
-          "storageInGB": "unknown"
-        }
-        node['geo'] = {
-            "name": "geolocation",
-            "type": "Geolocation",
-            "id": "geolocationUUID",
-        }
-
-    return nodes
+    node['objectType'] = objectType #TODO
+    node['os'] = ''
+    node['domain'] = ''
+    node['deviceDetails'] = {
+        "cpuType": "unknown",
+        "cpuCores": "unknown",
+        "memoryInMB": "unknown",
+        "storageInGB": "unknown"
+    }
+    node['geo'] = {
+        "name": "geolocation",
+        "type": "Geolocation",
+        "id": "geolocationUUID",
+    }
+    return node
 
 def live_graph_generate_nodes(nodes, json_file):
-
-    incoming_data = {}
-    outgoing_data = {}
-    incoming_connections = {}
-    outgoing_connections = {}
-    networks = {}
-    
-    networks_response = get_networks()
-
-    for item in networks_response['items']:
-        addresses = []
-        addr, mask = item['value'].split('/')
-
-        #TODO:find a better way to filter
-        if mask != '24':
-            continue
-        
-        network = IPNetwork(item['value'])
-        generator = network.iter_hosts()
-        for elem in generator:
-            addresses.append(str(elem))
-        
-        networks[item['value']] = addresses
-
-    for item in json_file['items']:
-        if item['destinationIp'] not in incoming_data.keys():
-            incoming_data[item['destinationIp']] = item['bytesSent']
-            incoming_connections[item['destinationIp']] = 0 
-        else:
-            incoming_data[item['destinationIp']] += item['bytesSent']
-            incoming_connections[item['destinationIp']] +=  1
-
-        if item['sourceIp'] not in outgoing_data.keys():
-            outgoing_data[item['sourceIp']] = item['bytesSent']
-            outgoing_connections[item['sourceIp']] = 0 
-        else:
-            outgoing_data[item['sourceIp']] += item['bytesSent']
-            outgoing_connections[item['sourceIp']] += 1
-
-    for node in nodes:
-        del node['connections']
-        del node['incoming']
-        del node['outgoing']
-        del node['svg']
-
-        node['status'] = "online"
-        node['ip'] = node['id']
-        node['id'] = 'HostUUID'
-        #we can't get hostUUID as of now
-
-        #initialization
-        node['nodeName'] = ''
-        protocol = ''
-        sourcePort = ''
-        node['macAddress'] = ''
-        for item in json_file['items']:
-            if item['sourceIp'] == node['ip']:
-                node['macAddress'] = item['srcMAC']
-                protocol = item['protocol']
-                sourcePort = item['sourcePort']
-                if node['nodeName'] == '':
-                    node['nodeName'] = item['hostName']
-            if item['destinationIp'] == node['ip']:
-                node['macAddress'] = item['dstMAC']
-
-        addresses = []
-
-        network = IPNetwork('/'.join([node['ip'], '24']))
-        #TODO:find proper mask
-        generator = network.iter_hosts()
-        for elem in generator:
-            addresses.append(str(elem))
-
-        lst_ip = addresses[0].split('.') #TODO
-        lst_ip[-1] = '0'
-
-        node['ip_scope'] = '.'.join(lst_ip)
-        node['ipGateway'] = addresses[0]
-        node['networkGroup'] = ''
-
-        for net in networks.keys():
-            if node['ip'] in networks[net]:
-                node['networkGroup'] = net
-
-        if node['ip'] in incoming_data.keys():
-            node['l_incomingData'] = incoming_data[node['ip']]
-            node['l_incomingConn'] = incoming_connections[node['ip']]
-        else:
-            node['l_incomingData'] = 0
-            node['l_incomingConn'] = 0
-
-        if node['ip'] in outgoing_data.keys():
-            node['l_outgoingData'] = outgoing_data[node['ip']]
-            node['l_outgoingConn'] = outgoing_connections[node['ip']]
-        else:
-            node['l_outgoingData'] = 0
-            node['l_outgoingConn'] = 0
-
-        try:
-            sourcePort = int(sourcePort)
-        except:
-            sourcePort = ''
-
-        node['port'] = {
-            "id": "protocoPortObjUUID",
-            "name": "protocolport_obj",
-            "type": "ProtocolPortObject",
-            "protocol": protocol,
-            "port": sourcePort
-          }
-
-        if (node['l_outgoingConn'] == 0 and node['l_incomingConn'] > 0 and node['l_outgoingData'] == 0):
-            objectType = 'Server'
-        else:
-            objectType = 'Client'
-
-        node['objectType'] = objectType #TODO
-        node['os'] = ''
-        node['domain'] = ''
-        node['deviceDetails'] = {
-          "cpuType": "unknown",
-          "cpuCores": "unknown",
-          "memoryInMB": "unknown",
-          "storageInGB": "unknown"
-        }
-        node['geo'] = {
-            "name": "geolocation",
-            "type": "Geolocation",
-            "id": "geolocationUUID",
-        }
-
-    return nodes
+    return auto_graph_generate_nodes(nodes, json_file)
 
 def parse_traffic_line(line):
     line = " ".join(line.split())
@@ -317,11 +191,6 @@ def parse_traffic_line(line):
     return has_port, index, timestap, (source_ip, source_port), (destination_ip, destination_port), upd_length
 
 def convert_to_json(content):
-    '''
-        Ouput template (from https://www.tunnelsup.com/packet-captures-on-cisco-asa/):
-            HH:MM:SS.ms [ether-hdr] src-addr.src-port dest-addr.dst-port: tcp-flags [header-check] 
-            [checksum-info] sequence-number ack-number tcp-window urgent-info tcp-options
-    '''
     data = content["response"][0]
     data = html.unescape(data)
     with open("live_data.txt", "w") as f:
@@ -369,8 +238,7 @@ def colour_gradient(colour1, colour2, range):
 
     return colours
 
-
-def process_auto_graph_data(response_data, live_data, timeframe=10000):
+def process_data(response_data, live_data, timeframe=10000):
     temp_links = {}
 
     for line in live_data["items"]:
@@ -394,6 +262,10 @@ def process_auto_graph_data(response_data, live_data, timeframe=10000):
                 break
         if known_traffic is False:
             link["value"] = 0
+    return response_data, max_udp_length
+
+def process_auto_graph_data(response_data, live_data, timeframe=10000):
+    response_data, max_udp_length = process_data(response_data, live_data, timeframe)
 
     for link in response_data["links"]:
         if link["value"] > 0:
@@ -405,36 +277,12 @@ def process_auto_graph_data(response_data, live_data, timeframe=10000):
     return response_data
 
 def process_live_data(response_data, live_data, timeframe=10000):
-
-    temp_links = {}
-
-    for line in live_data["items"]:
-        sourceIp, destinationIp = line["sourceIp"], line["destinationIp"]
-        if f"{sourceIp}.{destinationIp}" not in temp_links:
-            temp_links[f"{sourceIp}_{destinationIp}"] = line["bytesSent"]
-        else:
-            temp_links[f"{sourceIp}_{destinationIp}"] += line["bytesSent"]
-
-    max_udp_length = 0
-    for link in response_data["links"]:
-        known_traffic = False
-        for ip_key, udp_length in temp_links.items():
-            source = ip_key.split("_")[0]
-            target = ip_key.split("_")[1]
-            if link["source"] == source and link["target"] == target:
-                if udp_length > max_udp_length:
-                    max_udp_length = udp_length
-                link["value"] = udp_length
-                known_traffic = True
-                break
-        if known_traffic is False:
-            link["value"] = 0
+    response_data, max_udp_length = process_data(response_data, live_data, timeframe)
 
     for link in response_data["links"]:
         if link["value"] > 0:
             link["CurrentTafficSize"] = link["value"]
-            link["value"] = transf_to_interval(link["value"], max_udp_length)            
-            #TODO:check values (75 value currentTrafficSize 54/1441??)
+            link["value"] = transf_to_interval(link["value"], max_udp_length)
             link['trafficType'] = 'TCP'
             #TODO:there's multiple types of traffic for IP1:IP2 connection ???
             link['absolutePath'] = [link['source'], link['target']]

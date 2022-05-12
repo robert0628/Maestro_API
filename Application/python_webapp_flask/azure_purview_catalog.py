736a4d35e9aa3e0fc75d7c0fee0f3b6558e4e342
valeriuhudea <valeriu.hudea@gmail.com>
2022-05-12 21:03:07 +0300

    Python SDK Catalog All Operations except CSV operations finished with Request GET, POST, DELET

--- Application/python_webapp_flask/azure_purview_catalog.py
+++ Application/python_webapp_flask/azure_purview_catalog.py
@@ -1,11 +1,40 @@
-from operator import contains
-from queue import Empty
+## Info over GET, POST, DELETE Operations and data format requirements:
+# operations need to follow the strict Purview Catalog Python SDK operation naming convention for each class type, bellow is the full list.
+# Classes: glossary, entity, relationship, lineage, discovery, types and collection
+# each class coreleates to respective endpoint functionality
+# ex. Glossary class is available via /api/cui/purview/glossary with the operation as a required request param ex.: /api/cui/purview/types?operation=delete_type_by_name
+# Full operation list available in DOC: https://azuresdkdocs.blob.core.windows.net/$web/python/azure-purview-catalog/1.0.0b3/azure.purview.catalog.operations.html
+# Data objects that are passed with the requests to each operation, need to be wrapped with the respective operation function parameters by exact naming convention,
+# in this sense we will take for example the discovery operation suggest (check Azure Docs: DiscoveryOperations) which takes as operation function param "suggest_request"
+# for the data object in the request POST ../discovery?operation=suggest to be valid it will have to be wrapped as the prameter type, in this case the "suggest_request" is
+# a JSONType object, the exact data object passed will be ex.:
+# {
+#     "suggest_request": {
+#         "keywords": "DOD",
+#         "limit": 0
+#     } # JSONType Object
+# }
+# if the request is valid, the response will provide the respective Purview data as Content-Type: application/json, or the success of the operation as status code. 
+# Request data needs to follow specific formatting and the correct key pair value input where required.
+# -- potential changes - error handling response, return for Null data responses or csv IO response. 
+# ex. {
+#    "results": null
+#}
+# catch None responses and pass status_code in this case 200 OK or 204 No Content.
+# All results from Purview catalog operations are returned as a JSON Object with the response values inside the "results" property.
+# 
+#  Official DOC: https://docs.microsoft.com/en-us/python/api/azure-purview-catalog/azure.purview.catalog.operations?view=azure-python-preview
+#
+import inspect
+from datetime import datetime
+
 from azure.purview.catalog import PurviewCatalogClient
 from azure.identity import ClientSecretCredential
 from azure.core.exceptions import HttpResponseError
 from .azure_purview_operations import *
-import inspect
 
+
+# Environment variables - need to be secured
 tenantId = "ababe4b8-e9ca-48b1-910a-9a632c08bff2"
 clientId = "d7b2cf84-1728-4ebc-a7ed-f2a4f1fa8dfe"
 clientSecret = "a~Z8Q~i5zsaWLO5.HdPtxRSWNpyMaquqWadP7ajQ"
@@ -27,48 +56,43 @@ def purview_catalog():
         response["Error"] = err
         return response
 
-def purview_request_args(obj):    
-    args = {}
-    if obj is not None:
-        for key in obj:
-            if type(obj[key]) == 'list':
-                args["list"] = obj[key]
-            elif type(obj[key]) == 'dict':
-                args["object"] = obj[key]
-            elif type(obj[key]) == 'str':
-                if key == "guid":
-                    args["guid"] = key
-                elif key == "name":
-                    args["name"] = key
-        return args
-    else:
-        args = None
-        return args
+# def purview_request_args(obj):    
+#     args = {}
+#     if obj is not None:
+#         for key in obj:
+#             if type(obj[key]) == 'list':
+#                 args["list"] = obj[key]
+#             elif type(obj[key]) == 'dict':
+#                 args["object"] = obj[key]
+#             elif type(obj[key]) == 'str':
+#                     args["str"] = obj[key]
+#         return args
+#     else:
+#         args = None
+#         return args
 
 def purview_catalog_entity(operation, data):
     client = purview_client()
     entity = client.entity
     operation_params = {}
     response = {}
-
-    if operation in entity_operations:
-        pass
-    else:
-        response["Error"] = "Operation not Supported!"
-        return response
     try:
         operation_params = data
         method = getattr(entity, operation)
         method_details = inspect.signature(method)
         method_params = list(method_details.parameters)
-        if len(operation_params) == 0:
+        if operation_params is not None and len(operation_params) == 0:
             result = method()
             return result
-        elif len(operation_params) > 0:
+        elif operation_params is not None and len(operation_params) > 0:
             for param in method_params:
                 if param in operation_params:
                     # print(operation_params[param])
-                    result = method(*operation_params)
+                    result = method(**operation_params)
+                    return result
+        else:
+            response["Error"] = "Only JSON Data as Content-Type: application/json is valid."
+            return response
     except HttpResponseError as err:
             response["Error"] = err
             return response
@@ -78,26 +102,24 @@ def purview_catalog_glossary(operation, data):
     glossary = client.glossary
     operation_params = {}
     response = {}
-    
-    if operation in glossary_operations:
-        pass
-    else:
-        response["Error"] = "operation not Supported!"
-        return response
-    
     try:
         operation_params = data
         method = getattr(glossary, operation)
         method_details = inspect.signature(method)
         method_params = list(method_details.parameters)
-        if len(operation_params) == 0:
+        if operation_params is not None and len(operation_params) == 0:
             result = method()
             return result
-        elif len(operation_params) > 0:
+        elif operation_params is not None and len(operation_params) > 0:
             for param in method_params:
                 if param in operation_params:
                     # print(operation_params[param])
-                    result = method(*operation_params)
+                    result = method(**operation_params)
+                    print(result)
+                    return result
+        else:
+            response["Error"] = "Only JSON Data as Content-Type: application/json is valid."
+            return response
     except HttpResponseError as err:
             response["Error"] = err
             return response
@@ -106,25 +128,24 @@ def purview_catalog_discovery(operation, data):
     discovery = client.discovery
     operation_params = {}
     response = {}
-
-    if operation in discovery_operations:
-        pass
-    else:
-        response["Error"] = "operation not Supported!"
-        return response
     try:
         operation_params = data
         method = getattr(discovery, operation)
         method_details = inspect.signature(method)
         method_params = list(method_details.parameters)
-        if len(operation_params) == 0:
+        if operation_params is not None and len(operation_params) == 0:
             result = method()
             return result
-        elif len(operation_params) > 0:
+        elif operation_params is not None and len(operation_params) > 0:
             for param in method_params:
                 if param in operation_params:
                     # print(operation_params[param])
-                    result = method(*operation_params)
+                    result = method(**operation_params)
+                    print(result)
+                    return result
+        else:
+            response["Error"] = "Only JSON Data as Content-Type: application/json is valid."
+            return response
     except HttpResponseError as err:
             response["Error"] = err
             return response
@@ -134,26 +155,24 @@ def purview_catalog_lineage(operation, data):
     lineage = client.lineage
     operation_params = {}
     response = {}
-
-
-    if operation in lineage_operations:
-        pass
-    else:
-        response["Error"] = "operation not Supported!"
-        return response
     try:
         operation_params = data
         method = getattr(lineage, operation)
         method_details = inspect.signature(method)
         method_params = list(method_details.parameters)
-        if len(operation_params) == 0:
+        if operation_params is not None and len(operation_params) == 0:
             result = method()
             return result
-        elif len(operation_params) > 0:
+        elif operation_params is not None and len(operation_params) > 0:
             for param in method_params:
                 if param in operation_params:
                     # print(operation_params[param])
-                    result = method(*operation_params)
+                    result = method(**operation_params)
+                    print(result)
+                    return result
+        else:
+            response["Error"] = "Only JSON Data as Content-Type: application/json is valid."
+            return response
     except HttpResponseError as err:
             response["Error"] = err
             return response
@@ -163,26 +182,24 @@ def purview_catalog_relationship(operation, data):
     relationship = client.relationship
     operation_params = {}
     response = {}
-
-    if operation in relationship_operations:
-        pass
-    else:
-        response["Error"] = "operation not Supported!"
-        return response
-
     try:
         operation_params = data
         method = getattr(relationship, operation)
         method_details = inspect.signature(method)
         method_params = list(method_details.parameters)
-        if len(operation_params) == 0:
+        if operation_params is not None and len(operation_params) == 0:
             result = method()
             return result
-        elif len(operation_params) > 0:
+        elif operation_params is not None and len(operation_params) > 0:
             for param in method_params:
                 if param in operation_params:
                     # print(operation_params[param])
-                    result = method(*operation_params)
+                    result = method(**operation_params)
+                    print(result)
+                    return result
+        else:
+            response["Error"] = "Only JSON Data as Content-Type: application/json is valid."
+            return response
     except HttpResponseError as err:
             response["Error"] = err
             return response
@@ -192,25 +209,24 @@ def purview_catalog_types(operation, data):
     types = client.types
     operation_params = {}
     response = {}
-
-    if operation in types_operations:
-        pass
-    else:
-        response["Error"] = "operation not Supported!"
-        return response   
     try:
         operation_params = data
         method = getattr(types, operation)
         method_details = inspect.signature(method)
         method_params = list(method_details.parameters)
-        if len(operation_params) == 0:
+        if operation_params is not None and len(operation_params) == 0:
             result = method()
             return result
-        elif len(operation_params) > 0:
+        elif operation_params is not None and len(operation_params) > 0:
             for param in method_params:
                 if param in operation_params:
                     # print(operation_params[param])
-                    result = method(*operation_params)
+                    result = method(**operation_params)
+                    print(result)
+                    return result
+        else:
+            response["Error"] = "Only JSON Data as Content-Type: application/json is valid."
+            return response
     except HttpResponseError as err:
             response["Error"] = err
             return response
@@ -219,24 +235,24 @@ def purview_catalog_collection(operation, data):
     collection = client.collection
     operation_params = {}
     response = {}
-    if operation in collection_operations:
-        pass
-    else:
-        response["Error"] = "operation not Supported!"
-        return response
     try:
         operation_params = data
         method = getattr(collection, operation)
         method_details = inspect.signature(method)
         method_params = list(method_details.parameters)
-        if len(operation_params) == 0:
+        if operation_params is not None and len(operation_params) == 0:
             result = method()
             return result
-        elif len(operation_params) > 0:
+        elif operation_params is not None and len(operation_params) > 0:
             for param in method_params:
                 if param in operation_params:
                     # print(operation_params[param])
-                    result = method(*operation_params)
+                    result = method(**operation_params)
+                    print(result)
+                    return result
+        else:
+            response["Error"] = "Only JSON Data as Content-Type: application/json is valid."
+            return response
     except HttpResponseError as err:
             response["Error"] = err
             return response
